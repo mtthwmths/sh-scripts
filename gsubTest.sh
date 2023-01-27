9c10cad49d5885071728a38de7c3d7837a535934
Matt M <mtthwmths@gmail.com>
2023-01-27 08:59:44 -0600

    removes the \b for gawk word wrap
    gawk/awk uses \<, \>, and \y for word wraps. \b is backspace.

--- gsubTest.sh
+++ gsubTest.sh
@@ -14,10 +14,10 @@ touch $input_file
 touch $output_file
 
 #string of patterns that are not names
-exclude_strings="\\b(db|dbcp|http|jmx|editor|startheartbeatactivity|scriptrunner|createpagetemplate|jira|thread|resumedraft|atlassian|catalina|awt|java|sidebar|common|ForkJoinPool|session|pubsub|entity|Function|jdk|currentNode|CodeSmells|synchrony|version|HazelcastInstance|confluence|custom|doeditattachment|config|viewpageattachments|manifold|clojure|aleph|ginga|org|com|rome|application|bnd|editattachment|ProviderManager|letterhead|line|movepage|user|path|os|file|jnidispatch|stash|bitbucket|jna|uploadpack|atl|batch|form|table|package)\\b"
+exclude_strings="(db|dbcp|http|jmx|editor|startheartbeatactivity|scriptrunner|createpagetemplate|jira|thread|resumedraft|atlassian|catalina|awt|java|sidebar|common|ForkJoinPool|session|pubsub|entity|Function|jdk|currentNode|CodeSmells|synchrony|version|HazelcastInstance|confluence|custom|doeditattachment|config|viewpageattachments|manifold|clojure|aleph|ginga|org|com|rome|application|bnd|editattachment|ProviderManager|letterhead|line|movepage|user|path|os|file|jnidispatch|stash|bitbucket|jna|uploadpack|atl|batch|form|table|package)"
 
 #actual command
 gawk -v exclude="$exclude_strings" \
 '{if (! match($0, exclude))
-gsub(/([a-zA-Z])+\.([a-zA-Z0-9])+((\.)*([a-zA-Z0-9])*){0,1}/, "USER_REDACTED"); print}' \
+gsub(/\<.*\>/, "word"); print}' \
 $input_file > $output_file
